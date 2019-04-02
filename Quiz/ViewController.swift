4e20674710f3ed676894c64505412678819417a5
Samuel Kanenwisher <skanenwisher@gmail.com>
2019-04-01 21:33:22 -0700

    Table Views

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -8,16 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController, QuizProtocol {
+class ViewController: UIViewController, QuizProtocol, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    @IBOutlet weak var tableView: UITableView!
     
     var model = QuizModel()
-    var question = [Question]()
+    var questions = [Question]()
     var questionIndex = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Conform to the table view protocols
+        tableView.dataSource = self
+        tableView.delegate = self
         
+        // Set self as delegate for model and call get questions
         model.delegate = self
         model.getQuestions()
     }
@@ -25,8 +32,43 @@ class ViewController: UIViewController, QuizProtocol {
     // MARK: - QuizProtocol methods
     
     func questionsRetrieved(questions: [Question]) {
-        print("Hey Questions are returned!")
+        
+        
+        //Set our questions property with the questions from quiz model
+        self.questions = questions
+        
+        // Tell the tableview to reload data
+        tableView.reloadData()
+    }
+    
+    // MARK: - TableView Protocol Methods
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        guard questions.count > 0 && questions[questionIndex].answers != nil else {
+            return 0
+        }
+        
+        return questions[questionIndex].answers!.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AnswerCell", for: indexPath)
+        
+        // Get the label
+        let label = cell.viewWithTag(1) as! UILabel
+        
+        // TODO: Set the text for the label
+        
+        return cell
+        
     }
 
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        // User has selected an answer
+        
+    }
+    
 }
 
